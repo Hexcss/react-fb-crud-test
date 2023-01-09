a47f05fe9a0f8b2faa5ae45090129528a175d937
JavierCader <113170731+JavierCader@users.noreply.github.com>
2023-01-09 14:14:59 +0100

    REACT CRUD TEST

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import { db } from "./firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({
+    name: "",
+    age: 0,
+  });
+  const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, newUser);
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id);
+    const updatedFields = { age: age + 1 };
+    await updateDoc(userDoc, updatedFields);
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+
+  return (
+    <div className="container">
+      <input
+        placeholder="Name..."
+        value={newUser.name}
+        onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+      />
+
+      <input
+        placeholder="Age..."
+        value={newUser.age}
+        onChange={(e) =>
+          setNewUser({ ...newUser, age: Number(e.target.value) })
+        }
+      />
+
+      <button onClick={createUser}>Create User</button>
+
+      {users.map((user) => {
+        return (
+          <div className="user" key={user.id}>
+            <h1>Name: {user.name}</h1>
+            <h2>Age: {user.age}</h2>
+            <div className="buttons">
+              <button onClick={() => updateUser(user.id, user.age)}>
+                Increase Age
+              </button>
+              <button onClick={() => deleteUser(user.id)}>Delete User</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { API_KEY, APP_ID, AUTH_DOMAIN, MEASUREMENT_ID, MESSAGE_SENDER_ID, PROJECT_ID, STORAGE_BUCKET } from "./config/environment";
+
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGE_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
